e69240ab2181c54d69bf880f88e1476e7b3eeecc
abhi7985 <developer.gudh@gmail.com>
2025-12-16 23:11:41 +0530

    changes in database4

--- grocery_project/settings.py
+++ grocery_project/settings.py
@@ -92,24 +92,13 @@ WSGI_APPLICATION = "grocery_project.wsgi.application"
 
 # ---------- Database ----------
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-# if 'DATABASE_URL' in os.environ:
-#     DATABASES['default'] = dj_database_url.config(
-#         conn_max_age=600,
-#         ssl_require=True
-#     )
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
 
 # ---------- Password validation ----------
 
