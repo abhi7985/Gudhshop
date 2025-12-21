231d8186fbbc628be9b71c5e26eeb16af1fd2c96
abhi7985 <developer.gudh@gmail.com>
2025-12-21 23:56:10 +0530

    Changed the database2

--- grocery_project/settings.py
+++ grocery_project/settings.py
@@ -93,12 +93,12 @@ WSGI_APPLICATION = "grocery_project.wsgi.application"
 
 # ---------- Database ----------
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 DATABASES = {
     "default": dj_database_url.config(
         default=os.environ.get("DATABASE_URL"),
