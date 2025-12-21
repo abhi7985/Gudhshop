d1d78b110f5ea39cae09ad93d761fd42ad362a8d
abhi7985 <developer.gudh@gmail.com>
2025-12-21 23:56:42 +0530

    Changed the database3

--- grocery_project/settings.py
+++ grocery_project/settings.py
@@ -99,13 +99,13 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True,
-    )
-}
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.environ.get("DATABASE_URL"),
+#         conn_max_age=600,
+#         ssl_require=True,
+#     )
+# }
 
 
 # ---------- Password validation ----------
